d90aef70eddad7cf285f699fae4cd8aace9bfcfa
doubleyousee <double.you.see@gmail.com>
2023-12-28 14:31:09 -0500

    Day 8 Complete

--- AdventOfCode2015/Day8/Day8_1.ps1
+++ AdventOfCode2015/Day8/Day8_1.ps1
@@ -1,1 +1,19 @@
 $PuzzleData = Get-Content -Path "$PSScriptRoot\PuzzleInput_Day8.txt"
+$AllStringLiterals = 0
+$AllInMemory = 0
+ForEach($StringLiteral in $PuzzleData){
+    $SLL = $StringLiteral.Length
+    $InMemory = $StringLiteral -Replace '^"(.*)"$','$1'
+    $InMemory = $InMemory -Replace '\\\\','\'
+    $InMemory = $InMemory -Replace '\\"','"' 
+    $InMemory = $InMemory -Replace '\\x[a-fA-F0-9][a-fA-F0-9]','!'
+    $IML = $InMemory.Length
+    Write-Host "$StringLiteral adds $SLL minus $InMemory with a length $IML"
+    $AllStringLiterals = $AllStringLiterals + $SLL
+    $AllInMemory = $AllInMemory + $IML
+}
+$AllStringLiterals
+$AllInMemory
+$Total = $AllStringLiterals - $AllInMemory
+Set-Clipboard $Total
+$Total
--- AdventOfCode2015/Day8/Day8_2.ps1
+++ AdventOfCode2015/Day8/Day8_2.ps1
@@ -1,1 +1,19 @@
 $PuzzleData = Get-Content -Path "$PSScriptRoot\PuzzleInput_Day8.txt"
+$AllStringLiterals = 0
+$AllEncoded = 0
+ForEach($StringLiteral in $PuzzleData){
+    $SLL = $StringLiteral.Length
+    $Encoded = $StringLiteral -Replace '\\','\\'
+    $Encoded = $Encoded -Replace '"','\"' 
+    $Encoded = $Encoded -Replace '^"(.*)"$','\"$1\"'
+    #$Encoded = $Encoded -Replace '\\x[a-fA-F0-9][a-fA-F0-9]','!'
+    $EL = $Encoded.Length
+    Write-Host "$StringLiteral adds $SLL minus $Encoded with a length $EL"
+    $AllStringLiterals = $AllStringLiterals + $SLL
+    $AllEncoded = $AllEncoded + $EL + 2
+}
+$AllStringLiterals
+$AllEncoded
+$Total = $AllEncoded - $AllStringLiterals
+Set-Clipboard $Total
+$Total
