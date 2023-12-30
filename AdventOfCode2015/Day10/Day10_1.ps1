6e86daf258f2e8d55f776bd9d6429b3a64256ae1
doubleyousee <double.you.see@gmail.com>
2023-12-30 00:41:04 -0500

    Day 10 Complete

--- AdventOfCode2015/Day10/Day10_1.ps1
+++ AdventOfCode2015/Day10/Day10_1.ps1
@@ -1,1 +1,16 @@
 $PuzzleData = Get-Content -Path "$PSScriptRoot\PuzzleInput_Day10.txt"
+$i = 1
+$Iteration = $PuzzleData
+While($i -le 40){
+    $MatchGroups = [regex]::matches($Iteration,'((\d)\2*)')
+    $NewString = ""
+    ForEach($Group in $MatchGroups){
+        $NewString = $NewString + "$([string]$Group.Length)"
+        $NewString = $NewString + "$([string]($Group.Value.ToCharArray())[0])"
+    }
+    Write-Host "Pass $($i): Current String has a Length of $($Iteration.Length), the updated string Length is now $($NewString.Length)"
+    $Iteration = $NewString
+    $i++
+}
+Set-Clipboard $Iteration.Length
+$Iteration.Length
--- AdventOfCode2015/Day10/Day10_2.ps1
+++ AdventOfCode2015/Day10/Day10_2.ps1
@@ -1,1 +1,18 @@
-$PuzzleData = Get-Content -Path "$PSScriptRoot\PuzzleInput_Day10.txt"
+##Warning, this code will take HOURS, Powershell ftw!
+
+$PuzzleData = "3113322113" #Get-Content -Path "$PSScriptRoot\PuzzleInput_Day10.txt"
+$i = 1
+$Iteration = $PuzzleData
+While($i -le 50){
+    $MatchGroups = [regex]::matches($Iteration,'((\d)\2*)')
+    $NewString = ""
+    ForEach($Group in $MatchGroups){
+        $NewString = $NewString + "$([string]$Group.Length)"
+        $NewString = $NewString + "$([string]($Group.Value.ToCharArray())[0])"
+    }
+    Write-Host "Pass $($i): Current String has a Length of $($Iteration.Length), the updated string Length is now $($NewString.Length)"
+    $Iteration = $NewString
+    $i++
+}
+Set-Clipboard $Iteration.Length
+$Iteration.Length
