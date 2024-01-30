d8f1302fb65f12cd9d798c99cbc4fa86e19d0575
doubleyousee <double.you.see@gmail.com>
2024-01-30 14:44:59 -0500

    Day 12 changes

--- AdventOfCode2015/Day12/Day12_2.ps1
+++ AdventOfCode2015/Day12/Day12_2.ps1
@@ -1,1 +1,6 @@
-$PuzzleData = Get-Content -Path "$PSScriptRoot\PuzzleInput_Day12.txt" | ConvertFrom-JSON
+$PuzzleData = Get-Content -Path "$PSScriptRoot\PuzzleInput_Day12.txt" | ConvertFrom-JSON -NoEnumerate
+ForEach($Line in $PuzzleData){
+    $Line <#| Get-Member | Where-Object MemberType -match "NoteProperty" | Select-Object -ExpandProperty Name | ForEach-Object {
+        $Line.$_ | Get-Member | Where-Object MemberType -match "NoteProperty" | Select-Object -ExpandProperty Name
+    }#>
+}
